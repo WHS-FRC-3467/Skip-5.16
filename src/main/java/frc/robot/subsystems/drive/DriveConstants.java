ee4a422a96f04191b1e2d9c9102c24d6a2a9d0cc
CJendantix <cjendantix@proton.me>
2026-01-11 17:00:19 -0500

    Update ratios TODO: update couple ratio

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -90,9 +90,9 @@ public class DriveConstants {
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 3.8181818181818183;
 
-    private static final double kDriveGearRatio = 7.363636363636365;
-    private static final double kSteerGearRatio = 15.42857142857143;
-    private static final Distance kWheelRadius = Inches.of(2.167);
+    private static final double kDriveGearRatio = 6.0;
+    private static final double kSteerGearRatio = 24.0;
+    private static final Distance kWheelRadius = Inches.of(2);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
