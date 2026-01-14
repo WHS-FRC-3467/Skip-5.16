6ae51b3ed566a7d21693a795d346e2dd44d1f3c5
cabnit-rs <jkistler28@windhamsd.org>
2026-01-13 21:01:17 -0500

    addee indexer

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Skip-5.16";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "abe520c152a4690ea7756c984afb2dfdfc0d4a9f";
-  public static final String GIT_DATE = "2026-01-11 11:17:26 EST";
-  public static final String GIT_BRANCH = "Init";
-  public static final String BUILD_DATE = "2026-01-11 11:36:42 EST";
-  public static final long BUILD_UNIX_TIME = 1768149402369L;
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "7d10d425c87cc637dd83228233210ae33ca7587b";
+  public static final String GIT_DATE = "2026-01-13 19:29:45 EST";
+  public static final String GIT_BRANCH = "4-indexer-subsystem-init";
+  public static final String BUILD_DATE = "2026-01-13 20:57:58 EST";
+  public static final long BUILD_UNIX_TIME = 1768355878500L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
