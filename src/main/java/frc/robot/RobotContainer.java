ab191670efb15c21d7b173302c0b7c6044f211a9
Wilk <mjwilk25@gmail.com>
2026-01-17 12:33:40 -0500

    Updated Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -289,8 +289,7 @@ public class RobotContainer {
         controller.rightBumper().whileTrue(
             turret.shoot());
 
-        SmartDashboard.putData("HoodMovement", Commands
-            .runOnce(() -> turret.setHoodAngle(Degrees.of(100))));
+        SmartDashboard.putData("HoodMovement", turret.setHoodAngle(Degrees.of(100)));
 
         inAllianceRegionTrigger.onTrue(
             Commands.runOnce(() -> Logger.recordOutput("InAllianceRegionTrigger", true))
