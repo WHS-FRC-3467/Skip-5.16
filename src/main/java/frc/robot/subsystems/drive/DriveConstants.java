774054390cbe2fea96c91a9fe5381f0f6e85c8ac
spybh66 <brysonh99@gmail.com>
2026-01-21 11:25:04 -0500

    Update drive constants and tuning parameters for sim

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -80,11 +80,11 @@ public class DriveConstants {
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
-    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.69);
+    public static final LinearVelocity kSpeedAt12Volts = FeetPerSecond.of(17.5);
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
-    private static final double kCoupleRatio = (36 / 16);
+    private static final double kCoupleRatio = (36.0 / 16.0);
 
     private static final double kDriveGearRatio = 6.0;
     private static final double kSteerGearRatio = 24.0;
