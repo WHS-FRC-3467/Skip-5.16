10f1c117608e17c30b6f453fe39fde351afbc66b
jw00541 <wightjjohn@gmail.com>
2026-01-25 20:09:43 -0500

    Proposed architecture

--- src/main/java/frc/robot/commands/autos/PreloadShootAuto.java
+++ src/main/java/frc/robot/commands/autos/PreloadShootAuto.java
@@ -18,35 +18,54 @@ package frc.robot.commands.autos;
 import org.littletonrobotics.junction.Logger;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.indexer.Indexer.State;
+import frc.robot.subsystems.turret.ShooterSuperstructure;
+import frc.robot.RobotState;
 
+/*
+ * Auto moves robot to a safe shooting position and then shoots all preloaded fuel.
+ */
 public class PreloadShootAuto extends AutoCommands {
 
-    private PathPlannerPath driveToShot = null;
+    private PathPlannerPath driveToShot;
 
-    public PreloadShootAuto(Drive drive, Indexer indexer)
+    public PreloadShootAuto(Drive drive, Indexer indexer, ShooterSuperstructure shooter)
     {
         try {
-            // TODO: actual path -- deploy\pathplanner
-            driveToShot = PathPlannerPath.fromPathFile("PreloadShootAuto");
-
+            RobotState robotState = RobotState.getInstance();
+            driveToShot = PathPlannerPath.fromPathFile("PreloadShootAuto"); // TODO: actual path
+            // Margin of error on PathPlanner ending pose for determining indexer bring-up
+            Pose2d shotPose = driveToShot.getPathPoses().get(driveToShot.getPathPoses().size() - 1);
+            Trigger atShotPose = new Trigger(
+                () -> (robotState.getEstimatedPose().getTranslation()
+                    .getDistance(shotPose.getTranslation())) < 0.15); // TODO: acceptance radius
 
             if (Robot.isSimulation() && !Logger.hasReplaySource()) {
                 addCommands(AutoCommands.resetOdom(drive, driveToShot));
             }
 
+            // Drive to shooting location while spinning up shooter but not feeding. Don't run
+            // indexer until within shotPose acceptance radius. Once in range, spin up indexer to
+            // begin feeding & shooting. Shoot preload over timeout. Fallback: if path finding fails
+            // within 5s, attempt shot anyway and move on
             addCommands(Commands.sequence(
-
-                // Drive to shooting location while spinning up shooter
                 Commands.parallel(
-                    AutoBuilder.followPath(driveToShot))), // TODO: Add shooter/tower spin-up e.g.
-                                                           // shooter.spinUpCommand()
-                Commands.waitUntil(() -> true)); // TODO:
+                    new ParallelDeadlineGroup(AutoBuilder.followPath(driveToShot),
+                        shooter.prepareShot(Commands.none())).withTimeout(5.0)),
+                shooter.prepareShot(indexer.holdStateUntilInterrupted(State.PULL))
+                    .onlyWhile(atShotPose)
+                    .withTimeout(1.0))); // TODO: beambreak feedback or confirmed bps for timer
         } catch (Exception e) {
+            DriverStation.reportError("Failed to load PreloadShootAuto: " + e.getMessage(),
+                e.getStackTrace());
         }
     }
 }
-
