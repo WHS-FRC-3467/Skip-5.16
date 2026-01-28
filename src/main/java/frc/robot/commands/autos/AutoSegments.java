952d62e3b86b6ea0ac923896f11d97c1dfeef938
jw00541 <wightjjohn@gmail.com>
2026-01-28 14:17:06 -0500

    Architecture check

--- src/main/java/frc/robot/commands/autos/AutoSegments.java
+++ src/main/java/frc/robot/commands/autos/AutoSegments.java
@@ -15,29 +15,27 @@
 
 package frc.robot.commands.autos;
 
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import org.littletonrobotics.junction.Logger;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import frc.robot.Robot;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.indexer.Indexer.State;
+import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.turret.ShooterSuperstructure;
 
-/*
- * Auto moves robot to a safe shooting position and then shoots all preloaded fuel.
- */
-public class PreloadShootAuto extends AutoCommands {
+// Class containing command sequences that can be chained together in different configurations for
+// different autos
+public class AutoSegments {
 
-    private PathPlannerPath driveToShot;
-
-    public PreloadShootAuto(Drive drive, Indexer indexer, ShooterSuperstructure shooter,
-        StartPosition start)
+    // Follow a path and shoot preload
+    public static Command makePreloadShot(Drive drive, Indexer indexer,
+        ShooterSuperstructure shooter, StartPosition start)
     {
+        PathPlannerPath driveToShot;
         try {
             String pathName = switch (start) {
                 case LEFT -> "PreloadShoot-Left";
@@ -45,29 +43,30 @@ public class PreloadShootAuto extends AutoCommands {
                 case RIGHT -> "PreloadShoot-Right";
             };
             driveToShot = PathPlannerPath.fromPathFile(pathName);
-
-            if (Robot.isSimulation() && !Logger.hasReplaySource()) {
-                addCommands(AutoCommands.resetOdom(drive, driveToShot));
-            }
-            // Drive to shooting location while spinning up shooter but not indexing. Once at
-            // position, with the shooter still spinning, bring up the indexer to begin shooting.
-            // Shoot all preload. Bring down shooter and indexer to end. If path doesn't complete in
-            // 1.2x path time, attempt a shot anyway.
-            addCommands(Commands.sequence(
-                new ParallelDeadlineGroup(
-                    AutoBuilder.followPath(driveToShot),
-                    shooter.spinUpShooter()).withTimeout(2.5),
-                new ParallelDeadlineGroup(
-                    Commands.waitSeconds(1.0), // TODO: beam-break feedback or confirm bps for timer
-                    shooter.spinUpShooter(),
-                    indexer.holdStateUntilInterrupted(State.PULL)
-                        .onlyWhile(shooter.readyToShoot())),
-                shooter.setFlyWheelSpeed(RadiansPerSecond.zero()),
-                indexer.holdStateUntilInterrupted(State.STOP)));
-
         } catch (Exception e) {
             DriverStation.reportError("Failed to load PreloadShootAuto: " + e.getMessage(),
                 e.getStackTrace());
+            return Commands.none();
         }
+        // Drive to shooting location while spinning up shooter but not indexing. Once at
+        // position, with the shooter still spinning, bring up the indexer to begin shooting.
+        // Shoot all preload. Bring down indexer to end -- shooter will idle at speed. If path
+        // doesn't complete in 1.2x path time, attempt a shot anyway.
+        return Commands.sequence(
+            new ParallelDeadlineGroup(
+                AutoBuilder.followPath(driveToShot),
+                shooter.spinUpShooter()).withTimeout(2.5),
+            new ParallelDeadlineGroup(
+                Commands.waitSeconds(1.0), // TODO: beam-break feedback or confirm bps for timer
+                shooter.spinUpShooter(),
+                indexer.holdStateUntilInterrupted(State.PULL)
+                    .onlyWhile(shooter.readyToShoot())),
+            indexer.holdStateUntilInterrupted(State.STOP));
+    }
+
+    // Make a run for the neutral zone
+    public static Command makeNeutralRun(Drive drive, Intake intake, StartPosition start)
+    {
+        return Commands.none();
     }
 }
---
+++ src/main/java/frc/robot/commands/autos/PreloadNeutralAuto.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2026 Windham Windup
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+
+package frc.robot.commands.autos;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.lib.util.AutoCommand;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.turret.ShooterSuperstructure;
+
+// Auto routine that utilizes AutoSegments commands to shoot a preload, collect pieces from the
+// neutral zone, and then shoot them
+public class PreloadNeutralAuto extends AutoCommand {
+    private final List<String> pathNames;
+    private final List<PathPlannerPath> pathsUsed = new ArrayList<>();
+
+    public PreloadNeutralAuto(Drive drive, Intake intake, Indexer indexer,
+        ShooterSuperstructure shooter, StartPosition start)
+    {
+        // Choose path names based on start position
+        pathNames = switch (start) {
+            case LEFT -> List.of("PreloadShoot-Left", "Neutral-Left");
+            case CENTER -> List.of("PreloadShoot-Center", "Neutral-Center");
+            case RIGHT -> List.of("PreloadShoot-Right", "Neutral-Right");
+        };
+
+        // Load PathPlanner paths based on path names
+        loadPaths();
+
+        // Chain AutoSegments here
+        addCommands(
+            AutoSegments.makePreloadShot(drive, indexer, shooter, start)
+        // Autosegments.makeNeutralRun()
+        );
+
+    }
+
+    @Override
+    public List<Pose2d> getAllPathPoses()
+    {
+        return pathsUsed.stream().flatMap(path -> path.getPathPoses().stream()).toList();
+    }
+
+    @Override
+    public Pose2d getStartingPose()
+    {
+        return pathsUsed.get(0).getStartingHolonomicPose().get();
+    }
+
+    // Load the PathPlanner paths based on determined path names
+    private void loadPaths()
+    {
+        for (String name : pathNames) {
+            try {
+                pathsUsed.add(PathPlannerPath.fromPathFile(name));
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to load PathPlanner path: " + name);
+            }
+        }
+    }
+}
