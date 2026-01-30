c793d98435fe04ecf96717a1ceddcc092c9d7381
cabnit-rs <jkistler28@windhamsd.org>
2026-01-29 19:57:51 -0500

    kicker roller init

---
+++ src/main/java/frc/robot/subsystems/indexer/KickerRoller.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2026 Windham Windup
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+
+package frc.robot.subsystems.indexer;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.io.motor.MotorIO.PIDSlot;
+import frc.lib.mechanisms.flywheel.FlywheelMechanism;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+public class KickerRoller extends SubsystemBase {
+    private final FlywheelMechanism<?> io;
+    private State state = State.STOP;
+
+    @RequiredArgsConstructor
+    @SuppressWarnings("Immutable")
+    @Getter
+    public enum State {
+        STOP(
+            () -> RotationsPerSecond.zero()),
+        IDLE(
+            () -> RotationsPerSecond.of(1.0)),
+        SHOOT(
+            () -> KickerRollerConstants.MAX_VELOCITY);
+
+        private final Supplier<AngularVelocity> stateVelocity;
+    }
+
+    public KickerRoller(FlywheelMechanism<?> io)
+    {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic()
+    {
+        Logger.recordOutput("KickerRoller/State", this.state.name());
+        io.periodic();
+    }
+
+    private void setState(State state)
+    {
+        this.state = state;
+        io.runVelocity(state.stateVelocity.get(),
+            KickerRollerConstants.MAX_ACCELERATION, PIDSlot.SLOT_0);
+    }
+
+    /**
+     * Sets the subsystem's state
+     * 
+     * In a sequence, this command is non-blocking (finishes instantly), but still requires the
+     * subsystem (you cannot set the subsystem's state twice in a {@link ParallelCommandGroup}))
+     * 
+     * @param state The state to hold
+     * @return The command sequence
+     */
+    public Command setStateCommand(State state)
+    {
+        return this.runOnce(() -> setState(state))
+            .withName(state.name());
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/indexer/KickerRollerConstants.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2026 Windham Windup
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+
+package frc.robot.subsystems.indexer;
+
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Second;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.MomentOfInertia;
+import frc.lib.io.motor.MotorIO;
+import frc.lib.io.motor.MotorIO.PIDSlot;
+import frc.lib.io.motor.MotorIOTalonFX;
+import frc.lib.io.motor.MotorIOTalonFXSim;
+import frc.lib.mechanisms.flywheel.FlywheelMechanism;
+import frc.lib.mechanisms.flywheel.FlywheelMechanismReal;
+import frc.lib.mechanisms.flywheel.FlywheelMechanismSim;
+import frc.lib.util.PID;
+import frc.robot.Constants;
+import frc.robot.Ports;
+import frc.robot.Robot;
+
+public class KickerRollerConstants {
+    public static String NAME = "KickerRoller";
+
+    public static final AngularVelocity MAX_VELOCITY =
+        Units.RadiansPerSecond.of(2 * Math.PI);
+    public static final AngularAcceleration MAX_ACCELERATION = MAX_VELOCITY.per(Second);
+
+
+    private static final double GEARING = (2.0 / 1.0);
+
+    public static final AngularVelocity TOLERANCE = MAX_VELOCITY.times(0.2);
+
+    private static final DCMotor DCMOTOR = DCMotor.getKrakenX60(1);
+    public static final MomentOfInertia MOI = KilogramSquareMeters.of(0.01);
+
+    // Velocity PID
+    public static final PID SLOT0_PID = new PID(80.0, 0.0, 0.0)
+        .withV(10.0);
+
+    public static TalonFXConfiguration getFXConfig()
+    {
+        TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.CurrentLimits.SupplyCurrentLimitEnable = Robot.isReal();
+        config.CurrentLimits.SupplyCurrentLimit = 40.0;
+        config.CurrentLimits.SupplyCurrentLowerLimit = 40.0;
+        config.CurrentLimits.SupplyCurrentLowerTime = 0.1;
+
+        config.CurrentLimits.StatorCurrentLimitEnable = Robot.isReal();
+        config.CurrentLimits.StatorCurrentLimit = 80.0;
+
+        config.Voltage.PeakForwardVoltage = 12.0;
+        config.Voltage.PeakReverseVoltage = -12.0;
+
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+        config.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
+
+        config.SoftwareLimitSwitch.ReverseSoftLimitEnable = false;
+
+        config.Feedback.RotorToSensorRatio = 1.0;
+
+        config.Feedback.SensorToMechanismRatio = GEARING;
+
+        config.Slot0 = Slot0Configs.from(SLOT0_PID.toSlotConfigs());
+
+        return config;
+    }
+
+    public static KickerRoller get()
+    {
+        FlywheelMechanism<?> mechanism;
+        switch (Constants.currentMode) {
+            case REAL:
+                mechanism = new FlywheelMechanismReal(NAME,
+                    new MotorIOTalonFX(NAME, getFXConfig(), Ports.indexer));
+                break;
+            case SIM:
+                mechanism = new FlywheelMechanismSim(NAME,
+                    new MotorIOTalonFXSim(NAME, getFXConfig(), Ports.indexer), DCMOTOR, MOI,
+                    TOLERANCE);
+                break;
+            case REPLAY:
+                mechanism = new FlywheelMechanism<>(NAME, new MotorIO() {}) {};
+                break;
+            default:
+                throw new IllegalStateException("Unrecognized Robot Mode");
+        }
+        mechanism.enableTunablePID(PIDSlot.SLOT_0, SLOT0_PID);
+        return new KickerRoller(mechanism);
+    }
+}
