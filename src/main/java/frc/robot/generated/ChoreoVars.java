c0558ad03d6b5f41a9c4266f91b18b4cdf635ba3
CJendantix <cjendantix@proton.me>
2026-03-10 12:44:21 -0400

    Switch to choreo code generation

---
+++ src/main/java/frc/robot/generated/ChoreoVars.java
@@ -0,0 +1,24 @@
+package frc.robot.generated;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.*;
+
+/**
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
+ */
+public final class ChoreoVars {
+
+    public static final class Poses {
+        public static final Pose2d NeutralShoot = new Pose2d(3.5, 7.4, Rotation2d.fromRadians(-1.25));
+        public static final Pose2d TunnelEntrance = new Pose2d(4, 7.399, Rotation2d.fromRadians(-1.571));
+        public static final Pose2d TunnelExit = new Pose2d(5.65, 7.399, Rotation2d.fromRadians(-1.571));
+
+        private Poses() {}
+    }
+
+    private ChoreoVars() {}
+}
