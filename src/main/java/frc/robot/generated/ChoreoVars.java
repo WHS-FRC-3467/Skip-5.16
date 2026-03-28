e4654f4f084370026dfd50112c73bb181d645dbe
jw00541 <wightjjohn@gmail.com>
2026-03-28 00:23:16 -0400

    Initial thinking on safe paths

--- src/main/java/frc/robot/generated/ChoreoVars.java
+++ src/main/java/frc/robot/generated/ChoreoVars.java
@@ -10,13 +10,13 @@ import edu.wpi.first.units.measure.*;
  * in the Choreo GUI.
  */
 public final class ChoreoVars {
+    public static final Distance Neutral2ML_Safe_Y = Units.Meters.of(3.9);
 
     public static final class Poses {
-        public static final Pose2d NeutralShoot = new Pose2d(3.125, 7.309, Rotation2d.fromRadians(-1.154));
-        public static final Pose2d TunnelEntrance = new Pose2d(4, 7.399, Rotation2d.fromRadians(-1.571));
-        public static final Pose2d TunnelExit = new Pose2d(5.65, 7.399, Rotation2d.fromRadians(-1.571));
-
-        private Poses() {}
+        public static final Pose2d DecisionPose = new Pose2d(6.75, 6.8, Rotation2d.fromRadians(-1.5707963));
+        public static final Pose2d NeutralShoot = new Pose2d(3.12514, 7.3089199, Rotation2d.fromRadians(-1.1536837));
+        public static final Pose2d TunnelEntrance = new Pose2d(4, 7.3992772, Rotation2d.fromRadians(-1.5707963));
+        public static final Pose2d TunnelExit = new Pose2d(5.65, 7.3992772, Rotation2d.fromRadians(-1.5707963));
     }
 
     private ChoreoVars() {}
